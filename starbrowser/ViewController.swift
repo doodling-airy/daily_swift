c7a9eb54360815c45bb225eae2141211a2bab32c
airy <thanks.xa@gmail.com>
2019-12-27 22:10:05 +0900

    added tableview and delegate, datasource

--- starbrowser/ViewController.swift
+++ starbrowser/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewControllera: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private var table: UITableView!
+    let vmList = viewmodel_List()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        table.delegate = self
+        table.dataSource = self
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.view.addSubview(table)
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return vmList.count()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel!.text = vmList.check()[indexPath.row]
+        return cell!
+    }
+}
